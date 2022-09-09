83130a28db252142c588d3b5ffc721a4f98b9a96
Sean Sheng <s3sheng@gmail.com>
2022-09-08 17:13:17 -0700

    tracing example

--- examples/quickstart/service.py
+++ examples/quickstart/service.py
@@ -1,16 +1,25 @@
-import numpy as np
-
 import bentoml
-from bentoml.io import NumpyNdarray
+from bentoml.io import JSON
+from bentoml._internal.configuration.containers import BentoMLContainer
 
-iris_clf_runner = bentoml.sklearn.get("iris_clf:latest").to_runner()
+class EchoRunnable(bentoml.Runnable):
+    SUPPORTED_RESOURCES = ("cpu",)
+    SUPPORTS_CPU_MULTI_THREADING = True
 
-svc = bentoml.Service("iris_classifier", runners=[iris_clf_runner])
+    def __init__(self):
+        # load the model instance
+        self.tracer = BentoMLContainer.tracer_provider.get().get_tracer(__name__)
 
+    @bentoml.Runnable.method(batchable=False)
+    def echo(self, input_data: list[int]) -> list[int]:
+        with self.tracer.start_as_current_span("custom_span"):
+            import time
+            time.sleep(0.1)
+            return input_data
 
-@svc.api(
-    input=NumpyNdarray.from_sample(np.array([[4.9, 3.0, 1.4, 0.2]], dtype=np.double)),
-    output=NumpyNdarray(),
-)
-def classify(input_series: np.ndarray) -> np.ndarray:
-    return iris_clf_runner.predict.run(input_series)
+echo_runner = bentoml.Runner(EchoRunnable, name="echo_runner")
+svc = bentoml.Service("echo_service", runners=[echo_runner])
+
+@svc.api(input=JSON(), output=JSON())
+def analysis(input_text: list[int]) -> list[int]:
+    return echo_runner.echo.run(input_text)
