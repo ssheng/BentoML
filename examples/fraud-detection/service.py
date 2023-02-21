a816d5be6895f4f8817c096038c0b77aa0bbd4a5
Sean Sheng <s3sheng@gmail.com>
2023-02-21 03:17:42 -0800

    Update with inference graph example

--- examples/fraud-detection/service.py
+++ examples/fraud-detection/service.py
@@ -1,3 +1,4 @@
+import asyncio
 import bentoml
 import pandas as pd
 import numpy as np
@@ -5,18 +6,39 @@ import numpy as np
 from bentoml.io import PandasDataFrame, JSON
 from sample import sample_input
 
-fraud_model_runner = bentoml.xgboost.get("ieee-fraud-detection-lg:latest").to_runner()
+fraud_model_tiny_runner = bentoml.xgboost.get("ieee-fraud-detection-tiny:latest").to_runner()
+fraud_model_small_runner = bentoml.xgboost.get("ieee-fraud-detection-small:latest").to_runner()
+fraud_model_large_runner = bentoml.xgboost.get("ieee-fraud-detection-large:latest").to_runner()
 
 
-svc = bentoml.Service("fraud_detection", runners=[fraud_model_runner]) 
+svc = bentoml.Service("fraud_detection_graph", runners=[fraud_model_tiny_runner, fraud_model_small_runner, fraud_model_large_runner]) 
 
 input_spec = PandasDataFrame.from_sample(sample_input)
 
 @svc.api(input=input_spec, output=JSON())
 def is_fraud(input_df: pd.DataFrame):
-    results = fraud_model_runner.predict_proba.run(input_df)
+    results = fraud_model_large_runner.predict_proba.run(input_df)
     prediction = np.argmax(results, axis=1)  # 0 is not fraud, 1 is fraud
     return {
         "is_fraud": bool(prediction),
         "is_fraud_prob": results[:,1]
     }
+
+async def _is_fraud_async(
+    runner: bentoml.Runner,
+    input_df: pd.DataFrame,
+):
+    results = await runner.predict_proba.async_run(input_df)
+    prediction = np.argmax(results, axis=1)  # 0 is not fraud, 1 is fraud
+    return {
+        "is_fraud": bool(prediction),
+        "is_fraud_prob": results[:,1]
+    }
+
+@svc.api(input=input_spec, output=JSON())
+async def is_fraud_graph(input_df: pd.DataFrame):
+    return await asyncio.gather(
+        _is_fraud_async(fraud_model_tiny_runner, input_df),
+        _is_fraud_async(fraud_model_small_runner, input_df),
+        _is_fraud_async(fraud_model_large_runner, input_df),
+    )
